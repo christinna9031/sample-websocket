46d3b7701a2b25075a9dac7e818f24109d1ddd11
Kristýna Králová <christinna@hotmail.cz>
2021-04-27 02:34:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require("body-parser");
 const path = require('path');
 const { createServer } = require('http');
-
 
 const WebSocket = require('ws');
 
 const app = express();
-const apps = express();
-const PORT = 3000
 app.use(express.static(path.join(__dirname, '/public')));
-
-apps.use(bodyParser.json())
-apps.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
-
-apps.post("/hook", (req, res) => {
-  console.log(req.body) // Call your action on the request here
-  res.status(200).end() // Responding is important
-})
-
-apps.get("/hook", (req, res) => {
-  console.log('GET request received');
-  res.status(200).end() // Responding is important
-  res.send('This the simple Webhook server sample.  Use POST methods instead of GET.')
-})
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function (ws) {
   const id = setInterval(function () {
-    ws.send(JSON.stringify("Hello"));
-  }, 1000);
+    ws.send("Hello"});
+  }, 100);
   console.log('started client interval');
 
   ws.on('close', function () {
