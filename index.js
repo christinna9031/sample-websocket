9a92d34b3c042de538f7158927f02fe0d12b5e4a
Kristýna Králová <christinna@hotmail.cz>
2021-04-27 00:56:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const { createServer } = require('http');
 
+
 const WebSocket = require('ws');
 
 const app = express();
 const apps = express();
 const PORT = 3000
 app.use(express.static(path.join(__dirname, '/public')));
+
 apps.use(bodyParser.json())
 apps.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
-apps.post("/", (req, res) => {
+apps.post("/hook", (req, res) => {
   console.log(req.body) // Call your action on the request here
   res.status(200).end() // Responding is important
 })
